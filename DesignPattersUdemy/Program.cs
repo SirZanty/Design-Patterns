c937e501ccc6880d5c8a689bf27663eea8074a0f
Santy <sirzanty@gmail.com>
2023-02-17 08:32:15 -0500

    clase 2

--- DesignPattersUdemy/Program.cs
+++ DesignPattersUdemy/Program.cs
@@ -6,6 +6,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        SingleResponsibility3 singleResponsibility3 = new SingleResponsibility3();
+        singleResponsibility3.Run();
         OpenClosed4 openClosed4 = new OpenClosed4();
         openClosed4.Run();        
     }
